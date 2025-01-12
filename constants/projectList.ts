392a5b185661d7f241d05032e0307ecdc0e3d459
gajanansr <gajanan1055@gmail.com>
2025-01-13 04:37:05 +0530

    updated projects

--- constants/projectList.ts
+++ constants/projectList.ts
@@ -1,10 +1,10 @@
 export const projectList = [
   {
     logo: "/assets/project-logo/programming.png",
-    title: "codeFusion",
+    title: "CodeFusion",
     description:
       "Built a basic web-based code IDE using the MERN stack that enables users to sign up/sign in, and create projects with HTML, CSS, and JavaScript. The application provides a simple, user-friendly interface for writing and editing code, with a focus on minimalism and ease of use.",
-    projectUrl: "https://code-ide-mern-stack-frontend.vercel.app/login",
+    projectUrl: "https://codefusion-frontend.vercel.app/login",
     gitHubUrl: "https://github.com/gajanansr/Code-IDE-MERN-Stack",
     year: "August 2024",
     languages: [
@@ -22,6 +22,69 @@ export const projectList = [
       },
       {
         name: "TailwindCSS",
+      },
+    ],
+  },
+  {
+    logo: "/assets/project-logo/easy.png",
+    title: "EasyCode",
+    description:
+      "EasyCode is a tool designed to help coders, especially those learning algorithms, data structures, and competitive programming, better understand coding problems by simplifying complex problem descriptions.",
+    projectUrl: "https://www.easycode.live/",
+    gitHubUrl: "https://github.com/gajanansr/easycode",
+    year: "October 2024",
+    languages: [
+      {
+        name: "MongoDB",
+      },
+      {
+        name: "NextJs",
+      },
+      {
+        name: "ShadCN",
+      },
+      {
+        name: "TailwindCSS",
+      },
+    ],
+  },
+  {
+    logo: "/assets/project-logo/sort.png",
+    title: "SortingVisualizer",
+    description:
+      "Designed and implemented an interactive web application that visualizes various sorting algorithms, including Merge Sort, Quick Sort, Heap Sort, and Bubble Sort. The app features adjustable animation speeds, bar counts, and visual representation of the sorting process for better understanding.",
+    projectUrl: "https://sorting-visualizer-gsr.vercel.app/",
+    gitHubUrl: "https://github.com/gajanansr/sorting-visualizer",
+    year: "October 2024",
+    languages: [
+      {
+        name: "ReactJs",
+      },
+      {
+        name: "TailwindCSS",
+      },
+      {
+        name: "Algorithms",
+      },
+    ],
+  },
+  {
+    logo: "/assets/project-logo/datemutuals.png",
+    title: "DateMutuals",
+    description:
+      "I developed the DateMutuals landing page with Karun for the DateMutuals startup, using Next.js for the frontend, AppSheet API for database integration, and Tailwind CSS for responsive styling. The project aimed to create a modern, user-friendly interface that ensures smooth interactions and an engaging experience.",
+    projectUrl: "https://fascinating-taffy-0fc652.netlify.app/",
+    gitHubUrl: "https://github.com/singhalkarun/datemutuals-v2.git",
+    year: "October 2024",
+    languages: [
+      {
+        name: "NextJs",
+      },
+      {
+        name: "TailwindCSS",
+      },
+      {
+        name: "AppsheetAPI",
       },
     ],
   },
